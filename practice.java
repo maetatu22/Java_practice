76ff194e46f0383eb5434e9472855d0ddf7e27b4
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-13 02:48:48 +0900

    出力練習

---
+++ practice.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("こんにちは、java");
+    System.out.println(17);
+    System.out.println(5 + 3);
+    System.out.println("5+3");
+  }
+}
