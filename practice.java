7a12a62a041c1c2e7e1dc8dfa33fbd8d56ddcaee
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-14 01:22:38 +0900

    出力練習

--- practice.java
+++ practice.java
@@ -1,8 +1,20 @@
+import sun.net.www.content.text.plain;
+
+// import sun.net.www.content.text.plain;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("こんにちは、java");
     System.out.println(17);
     System.out.println(5 + 3);
     System.out.println("5+3");
+    System.out.println(12 / 3);
+    System.out.println(3 * 6);
+    System.out.println(8 % 3);
+    System.out.println("こんにちは"+"世界");
+    System.out.println("38"+"19");
+    System.out.println(38+19);
+    System.out.println();
   }
 }
+
