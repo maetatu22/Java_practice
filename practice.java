7112721cc5d9e3572552652e27dd7a2e0b832089
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-14 02:17:13 +0900

    コード練習

--- practice.java
+++ practice.java
@@ -1,20 +1,46 @@
-import sun.net.www.content.text.plain;
 
-// import sun.net.www.content.text.plain;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("こんにちは、java");
-    System.out.println(17);
-    System.out.println(5 + 3);
-    System.out.println("5+3");
-    System.out.println(12 / 3);
-    System.out.println(3 * 6);
-    System.out.println(8 % 3);
-    System.out.println("こんにちは"+"世界");
-    System.out.println("38"+"19");
-    System.out.println(38+19);
-    System.out.println();
+    // System.out.println("こんにちは、java");
+    // System.out.println(17);
+    // System.out.println(5 + 3);
+    // System.out.println("5+3");
+    // System.out.println(12 / 3);
+    // System.out.println(3 * 6);
+    // System.out.println(8 % 3);
+    // System.out.println("こんにちは"+"世界");
+    // System.out.println("38"+"19");
+    // System.out.println(38+19);
+    // int number1;
+    // number1 = 3;
+    // System.out.println(number1);
+    // String name;
+    // name = "Wanko";
+    // System.out.println(name);
+    // int number2;
+    // number2 = 7;
+    // System.out.println(number1*number2);
+    // String text;
+    // text = "プログラミングを勉強しよう";
+    // System.out.println("Progateで"+text);
+    // int number = 11;
+    // String text = "Ruby";
+    // System.out.println(number);
+    // System.out.println(text);
+    // number = 9;
+    // System.out.println(number);
+    // text = "Java";
+    // System.out.println(text);
+    // int number = 3;
+    // System.out.println(number);
+    // number = number + 7;
+    // System.out.println(number);
+    int number = 8;
+    number+= 7;
+    System.out.println(number);
+    number++;
+    System.out.println(number);
   }
 }
 
---
+++ practice2.java
@@ -0,0 +1,27 @@
+class Main2 {
+  public static void main(String[] args) {
+    int length = 6;
+    int height = 8;
+    
+    // 変数rectangleAreaに、四角形の面積を代入してください
+    int rectangleArea;
+    rectangleArea = length * height;
+    System.out.println(rectangleArea);
+    
+    // 変数rectangleAreaを出力してください
+    
+    
+    // 変数triangleAreaに、三角形の面積を代入してください
+    int triangleArea;
+    triangleArea = length * height /2;
+    System.out.println(triangleArea);
+    
+   
+    double number1;
+    number1 = 8.5;
+    double number2;
+    number2 = 3.4;
+    System.out.println(number1 + number2);
+    System.out.println(number2 - number2);
+  }
+}
