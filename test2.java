a67e0a2489db9ede076bbd2e8e84ca213344024d
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-22 20:02:44 +0900

    配列繰り返し

--- test2.java
+++ test2.java
@@ -24,8 +24,12 @@ class Test2 {
     String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
     
     // for文を用いて、「私の名前は◯◯です」と出力してください
-    for(int i = 0; i < names.length ; i++){
-      System.out.println("私の名前は"+names[i]+"です");
+    // for(int i = 0; i < names.length ; i++){
+    //   System.out.println("私の名前は"+names[i]+"です");
+    // }
+
+    for(String name: names){
+      System.out.println("私の名前は"+name+"です");
     }
   }
 }
