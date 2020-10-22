7a27f6790975f4b606c7493b8a5e5141f1e1c334
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-22 19:25:22 +0900

    配列値変更　練習

--- test2.java
+++ test2.java
@@ -1,13 +1,24 @@
 class Test2 {
   public static void main(String[] args) {
-    // 変数namesに、配列を代入してください
-    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+    // // 変数namesに、配列を代入してください
+    // String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
     
-    // インデックス番号が0の要素を出力してください
-    System.out.println(names[0]);
+    // // インデックス番号が0の要素を出力してください
+    // System.out.println(names[0]);
     
-    // インデックス番号が2の要素を出力してください
-    System.out.println(names[2]);
+    // // インデックス番号が2の要素を出力してください
+    // System.out.println(names[2]);
+
+
+    String[] languages = {"Ruby", "PHP", "Python"};
     
+      // インデックス番号が「1」の要素を出力してください
+      System.out.println(languages[1]);
+    
+      // インデックス番号が「1」の要素を「Java」で上書きしてください
+      languages[1]="java";
+      
+      // インデックス番号が「1」の要素を出力してください
+      System.out.println(languages[1]);
   }
 }
