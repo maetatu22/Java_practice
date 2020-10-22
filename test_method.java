0819a035d12d1dd0f414202b35e54a723caabb88
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-22 22:30:23 +0900

    メソッド練習

--- test_method.java
+++ test_method.java
@@ -1,3 +1,8 @@
-public class test_method {
-  
+class Test_mathod {
+  public static void main(String[] args) {
+    printData();
+  }
+  public static void printData(){
+    System.out.println("私の名前はKate Jonesです");
+  }   
 }
