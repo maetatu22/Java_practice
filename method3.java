9e74fb1ef7196b707b7e25f6d854d860e899b657
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-24 14:11:51 +0900

    メソッド練習3

---
+++ method3.java
@@ -0,0 +1,12 @@
+class  Method3 {
+  public static void main(String[] args){
+    printData("Kate Jones", 27);
+    printData("John Christopher Smith", 65);
+
+  }
+
+  public static void printData(String name, int age){
+    System.out.println("私の名前は"+name+"です");
+    System.out.println("私の年齢は"+age+"歳です");
+  }
+}
