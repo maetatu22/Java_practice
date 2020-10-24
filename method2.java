b72cf8f88a30300f39a6d4b9c1e725c62b02b3c0
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-24 14:04:29 +0900

    メソッド練習２

---
+++ method2.java
@@ -0,0 +1,11 @@
+class Method2 {
+  public static void main(String[] args){
+    printData("kate Jones");
+
+    printData("John Christopher Smith");
+  }
+
+  public static void printData(String name){
+    System.out.println("私の名前は"+name+"です");
+  }
+}
