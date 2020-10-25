68b508e778a774781192c2f479b5d42777a0a785
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-25 23:11:04 +0900

    メソッド6練習　雛形

---
+++ method6.java
@@ -0,0 +1,33 @@
+class Method6 {
+  public static void main(String[] args) {
+    printData(fullName("Kate", "Jones"), 27, 1.6, 50.0);
+    printData(fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
+  }
+
+  public static void printData(String name, int age, double height, double weight) {
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    
+    // 変数bmiに、bmiメソッドを用いてBMIを代入してください
+    
+    
+    // 変数bmiを用いて、「BMIは◯◯です」と出力してください
+    System.out.println();
+    
+  }
+
+  public static String fullName(String firstName, String lastName) {
+    return firstName + " " + lastName;
+  }
+  
+  public static String fullName(String firstName, String middleName, String lastName) {
+    return firstName + " " + middleName + " " + lastName;
+  }
+  
+  // bmiメソッドを定義してください
+  
+  
+}
+
