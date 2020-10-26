07f0dc1ac94cc4731cd4b097dda346c17fc7f44d
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-26 22:24:43 +0900

    メソッド練習7 問題雛形

---
+++ method7.java
@@ -0,0 +1,27 @@
+class Method7 {
+  public static void main(String[] args){
+    printData(fullName("Kate", "Jones"), 27,1.6,50.0);
+  }
+
+  public static void printData(String name, int age, double height ,double weight){
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    double bmi = bmi(height, weight);
+    System.out.println("BMIは" + bmi + "です");
+    
+  }
+
+  public static String fullName(String firstName, String lastName){
+    return firstName + " " + lastName;
+  }
+
+  public static String fullName(String firstName, String middleName, String ladtName){
+    return firstName + " " + middleName + " " + lastName;
+  }
+
+  public static double bmi(double height, double weight){
+    return weight / height/ height;
+  }
+}
