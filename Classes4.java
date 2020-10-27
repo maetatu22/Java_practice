9f86f58736c729969ff6a5816a0255531c8bc048
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-27 16:56:07 +0900

    Scanner練習

---
+++ Classes4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Classes4 {
+  public static void main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("名前： ");
+
+    String name = scanner.next();
+
+    System.out.println("こんにちは"+name+"さん");
+
+  }
+}
