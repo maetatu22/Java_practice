68ae1eed21a4839fe26d837f5caf8bb973a487d8
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-27 14:17:36 +0900

    クラス練習

---
+++ classes.java
@@ -0,0 +1,11 @@
+class classes {
+  public static void main(String[] args){
+    Person.hello();
+  }
+}
+
+class Person {
+  public static void hello() {
+    System.out.println("Hello Java");
+  }
+}
