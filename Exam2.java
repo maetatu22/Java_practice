ca390fc4fad88eaccdde2afe71ccb7e17bde919e
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-28 16:57:01 +0900

    道場問題4 回答

--- Exam2.java
+++ Exam2.java
@@ -7,10 +7,24 @@ class Exam2 {
 
     System.out.print("名前：");
     String firstName = scanner.next();
+
     System.out.print("名字：");
     String lastName = scanner.next();
+
+    System.out.print("年齢：");
+    int age = scanner.nextInt();
+
     String fullName = firstName + " " + lastName;
+
     System.out.println("名前は"+fullName+"です");
+
+    System.out.println("年齢は"+age+"歳です");
+    if(age >= 20){
+      System.out.println("成年者です");
+    }else if(age < 20){
+      System.out.println("未成年者です");
+    }
+    
 
 
   }
