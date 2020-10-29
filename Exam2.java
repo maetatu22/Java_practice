3ec1b5509195953272227959a9f9287a350115fb
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-29 23:46:00 +0900

    道場問題8　回答

--- Exam2.java
+++ Exam2.java
@@ -5,22 +5,33 @@ class Exam2 {
     
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("名前：");
-    String firstName = scanner.next();
+    System.out.print("何人分の情報を入力しますか：");
+    int multiple = scanner.nextInt();
 
-    System.out.print("名字：");
-    String lastName = scanner.next();
+    for(int i = 1; i < multiple; i++){
+      
+      System.out.println(i+"人目");
 
-    System.out.print("年齢：");
-    int age = scanner.nextInt();
+      System.out.print("名前：");
+      String firstName = scanner.next();
 
-    System.out.print("身長(m)：");
-    double height = scanner.nextDouble();
+      System.out.print("名字：");
+      String lastName = scanner.next();
 
-    System.out.print("体重(kg)：");
-    double weight = scanner.nextDouble();
+      System.out.print("年齢：");
+      int age = scanner.nextInt();
 
-    PersonExam2.printData(PersonExam2.fullName(firstName,lastName),age, height, weight); 
+      System.out.print("身長(m)：");
+      double height = scanner.nextDouble();
+
+      System.out.print("体重(kg)：");
+      double weight = scanner.nextDouble();
+
+      PersonExam2.printData(PersonExam2.fullName(firstName,lastName),age, height, weight);
+
+    }
+
+     
   }
 }
 
