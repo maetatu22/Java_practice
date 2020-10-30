acd2213eeeaaf3869c11eae8fc1e3b068e44b2b6
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-30 13:19:42 +0900

    オブジェクト　練習

---
+++ object.java
@@ -0,0 +1,12 @@
+class Object {
+  public static void main(String[] args){
+    personObject person = new personObject();
+    person.hello();
+  }
+}
+
+class personObject {
+  public void hello(){
+    System.out.println("こんにちは");
+  }
+}
