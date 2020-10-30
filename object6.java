c736d3fc00f6516b18556d600ab0719298e72272
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-30 14:21:11 +0900

    オブジェクト問題7

--- object6.java
+++ object6.java
@@ -1,15 +1,19 @@
 class Object6 {
   public static void main(String[] args){
-    personObject6 person1 = new personObject6();
-    personObject6 person2 = new personObject6();
+    personObject6 person1 = new personObject6("Kate Jones");
+    person1.hello();
+
+    personObject6 person2 = new personObject6("John Christopher Smith");
+    person2.hello();
   }
 }
 
 class personObject6 {
   public String name;
 
-  personObject6(){
+  personObject6(String name){
     System.out.println("インスタンスが生成されました");
+    this.name = name;
   }
 
   public void hello(){
