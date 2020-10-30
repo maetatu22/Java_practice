754eaa4582b26edc26d097220197f1d7c29d1dd4
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-30 13:26:22 +0900

    オブジェクト練習2

---
+++ object2.java
@@ -0,0 +1,11 @@
+class Object2 {
+  public static void main(String[] args){
+  personObject2 person1 = new personObject2();
+  personObject2 person2 = new personObject2();
+
+  }
+}
+
+class personObject2 {
+  
+}
