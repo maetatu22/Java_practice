0718edb1037c697e96f827f12c2495c3be8a47ae
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-31 13:04:28 +0900

    道場2問題2 回答

--- ExamSecond.java
+++ ExamSecond.java
@@ -1,6 +1,6 @@
 class ExamSecond  {
   public static void main(String[] args){
-    Bicycle bicycle1 = new Bicycle("ビアンキ");
+    Bicycle bicycle1 = new Bicycle("ビアンキ", "緑");
 
     System.out.println("【自転車の情報】");
     bicycle1.printData();
@@ -9,13 +9,16 @@ class ExamSecond  {
 
 class Bicycle {
   private String name;
+  private String color;
 
-  Bicycle(String name){
+  Bicycle(String name, String color){
     this.name = name;
+    this.color = color;
   }
 
   public void printData(){
     System.out.println("名前："+this.name);
+    System.out.println("色："+this.color);
   }
   
 }
