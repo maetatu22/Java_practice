6cc698490fad194801fca25fd3b1835b7feec512
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-31 13:08:39 +0900

    道場2問題3 回答

--- ExamSecond.java
+++ ExamSecond.java
@@ -1,6 +1,6 @@
 class ExamSecond  {
   public static void main(String[] args){
-    Bicycle bicycle1 = new Bicycle("ビアンキ", "緑");
+    Bicycle bicycle1 = new Bicycle("ビアンキ", "緑", 0);
 
     System.out.println("【自転車の情報】");
     bicycle1.printData();
@@ -10,15 +10,18 @@ class ExamSecond  {
 class Bicycle {
   private String name;
   private String color;
+  private int distance = 0;
 
-  Bicycle(String name, String color){
+  Bicycle(String name, String color, int distance){
     this.name = name;
     this.color = color;
+    this.distance = distance;
   }
 
   public void printData(){
     System.out.println("名前："+this.name);
     System.out.println("色："+this.color);
+    System.out.println("走行距離："+ this.distance + "km");
   }
   
 }
