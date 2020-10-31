f6af17f535926407103b51d09d70158917bfb948
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-31 13:48:00 +0900

    道場2問題5 回答

--- ExamSecond.java
+++ ExamSecond.java
@@ -1,12 +1,19 @@
-class ExamSecond  {
+import java.util.Scanner;
+
+class ExamSecond {
   public static void main(String[] args){
+
+    Scanner scanner = new Scanner(System.in);
+
     Bicycle bicycle1 = new Bicycle("ビアンキ", "緑", 0);
 
     System.out.println("【自転車の情報】");
     bicycle1.printData();
 
     System.out.println("-----------------");
-    bicycle1.run(10);
+    System.out.print("走る距離を入力してください：");
+    int mileage = scanner.nextInt();
+    bicycle1.run(mileage);
   }
 }
 
