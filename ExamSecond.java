fca9213e52143bfb0b5bc6b13ad4f945e1533217
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-01 18:11:35 +0900

    道場2問題7 回答 fuelフィールド追加

--- ExamSecond.java
+++ ExamSecond.java
@@ -18,7 +18,7 @@ class ExamSecond {
 
     System.out.println("=================");
 
-    Car car1 = new Car("フェラーリ", "赤", 0);
+    Car car1 = new Car("フェラーリ", "赤", 0, 100);
     
     System.out.println("【車の情報】");
     car1.printData();
@@ -54,17 +54,20 @@ class Car {
   private String name;
   private String color;
   private int distance = 0;
+  private int fuel = 100;
 
-  Car(String name, String color, int distance){
+  Car(String name, String color, int distance, int fuel){
     this.name = name;
     this.color = color;
     this.distance = distance;
+    this.fuel = fuel;
   }
 
   public void printData(){
     System.out.println("名前："+this.name);
     System.out.println("色："+this.color);
     System.out.println("走行距離："+this.distance+"km");
+    System.out.println("ガソリン量："+this.fuel+"L");
   }
 
   public void run(int mileage){
