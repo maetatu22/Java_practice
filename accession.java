243923893c1116bb7f903cbcd57d07156f907c6f
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-03 15:56:55 +0900

    継承練習

--- accession.java
+++ accession.java
@@ -5,37 +5,18 @@ class  Accession {
   }
 }
 
-class BicycleAccession {
-
-  private String name;
-  private String color;
-  private int distance = 0;
-
-  public String getName() {
-    return this.name;
-  }
-  public String getColor() {
-    return this.color;
-  }
-  public int getDistance() {
-    return this.distance;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public void setColor(String color) {
-    this.color = color;
-  }
-  public void printData() {
-    System.out.println("名前：" + this.name);
-    System.out.println("色：" + this.color);
-    System.out.println("走行距離：" + this.distance + "km");
-  }
+class BicycleAccession extends Vehicle{
+  
+  
 }
 
 
-class CarAccession {
+class CarAccession extends Vehicle{
 
+  
+}
+
+class Vehicle{
   private String name;
   private String color;
   private int distance = 0;
